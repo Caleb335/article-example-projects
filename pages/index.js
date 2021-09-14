be7553a14327ffdbd3a3d6b6806308b16330409b
Caleb <belac335@gmail.com>
2021-09-14 21:14:42 +0100

    feat: add header

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import Header from "../src/components/Header";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Caleb's article examples</title>
+        <link rel="icon" href="/img/codec.png" />
       </Head>
       <Header />
       <h1>All my article examples will go here</h1>
-    </div>
+    </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,51 @@
 import React from "react";
 import { navLinks } from "../utils/data";
+import Link from "next/link";
+import styled from "styled-components";
 
-export default Header = () => {
+const Navbar = styled.header`
+  height: 80px;
+  background: #3c1742;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding: var(--desktop-pad);
+
+  .brand h3 {
+    padding-top: 10px;
+  }
+
+  nav {
+    display: flex;
+  }
+
+  ul {
+    list-style: none;
+    padding-top: 10px;
+  }
+
+  li:hover {
+    cursor: pointer;
+  }
+`;
+
+export default function Header() {
   return (
-    <header>
-      <p>Examples</p>
-    </header>
+    <Navbar>
+      <div className="brand">
+        <h3>Example</h3>
+      </div>
+      <nav>
+        {navLinks.map((link, index) => {
+          return (
+            <ul>
+              <Link href={link.path}>
+                <li key={index}>{link.name}</li>
+              </Link>
+            </ul>
+          );
+        })}
+      </nav>
+    </Navbar>
   );
-};
+}
