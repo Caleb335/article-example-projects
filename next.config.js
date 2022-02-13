52e99138e892eca5ae619fc2f0902113bb426459
Caleb <belac335@gmail.com>
2022-02-14 00:47:19 +0100

    turn off eslint prod check

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
